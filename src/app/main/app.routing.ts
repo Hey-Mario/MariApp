7e2741934d2eded0eba094e59fc62bc59d99d7d6
Mario Lovamanitra <lovamanitra.randriamanantena@esti.mg>
2022-09-15 10:42:34 +0300

    Architecture 2.0

---
+++ src/app/main/app.routing.ts
@@ -0,0 +1,33 @@
+import { Router, RouterModule, Routes } from '@angular/router';
+import { ActuComponent } from './pages/actu/actu.component';
+import { AppComponent } from '../app.component';
+import { ErrorComponent } from './error/error.component';
+import { PostCommentsComponent } from './pages/posts/post-comments/post-comments.component';
+import { UserAlbumComponent } from './pages/profil/user-album/user-album.component';
+import { UserPostComponent } from './pages/profil/user-post/user-post.component';
+import { TodoListComponent } from './pages/todos/todo-list/todo-list.component';
+import { MainProfileComponent } from './pages/profil/main-profile/main-profile.component';
+import { LoginComponent } from '../account/login/login.component';
+import { UserPhotoComponent } from './pages/profil/user-photo/user-photo.component';
+
+const APP_ROUTING: Routes = [
+  // {path: '',component: AppComponent,
+  //   children: [
+    { path: '', redirectTo:'actu', pathMatch:"full" },
+    { path: 'login', component: LoginComponent },
+      { path: 'actu', component: ActuComponent },
+      { path: 'posts/:id/comments', component: PostCommentsComponent },
+      { path: 'todos/:id', component: TodoListComponent },
+      {path: 'profile/:id',component: MainProfileComponent,
+        children: [
+          { path: '', redirectTo:'posts', pathMatch:"full" },
+          { path: 'posts', component: UserPostComponent },
+          { path: 'albums', component: UserAlbumComponent },
+          { path: 'albums/:id', component: UserPhotoComponent },
+        ],
+      },
+    // ]},
+  { path: '**', component: ErrorComponent },
+];
+
+export const ROUTING = RouterModule.forRoot(APP_ROUTING);
